45cf2db7f2a573fd30c89f4d06293649e298d6dc
Ramya Kulkarni <ramyak@testvagrant.com>
2020-07-06 00:48:24 +0530

    JavaScript framework for API automation using cypress

---
+++ cypress/integration/tests/reqres-api.spec.js
@@ -0,0 +1,84 @@
+describe('REST API Automation', () => {
+    it('GET, Get all the Users Test API', () => {
+        cy.request('/api/users?page=2').then((response) => {
+            expect(response.status).equals(200)
+    })
+    });
+    
+    it('POST, CREATE a new User Test API', () => {
+    
+        let userDetails = {
+            "name": "ramya",
+            "job": "sdet"
+        }
+    
+        cy.request('POST','/api/users',userDetails).then((response) => {
+            expect(response.status).equals(201)
+    })
+    });
+    
+    it('PUT, Updates User details Test API', () => {
+    
+        let userDetails = {
+            "name": "rashmi",
+            "job": "software developer"
+        }
+    
+        cy.request('PUT','/api/users/2',userDetails).then((response) => {
+            expect(response.status).equals(200)
+            expect(response.body.name).equals('rashmi')
+            expect(response.body.job).equals('software developer')
+    })
+    });
+    
+    it('PATCH, Update job of a User details Test API', () => {
+    
+        let userDetails = {
+            "name": "rashmi",
+            "job": "Web Developer"
+        }
+    
+        cy.request('PATCH','/api/users/2',userDetails).then((response) => {
+            expect(response.status).equals(200)
+            expect(response.body.job).equals('Web Developer')
+    })
+    });
+    
+    it('DELETE, Delete a User Test API', () => {
+    
+        cy.request('DELETE','/api/users/2').then((response) => {
+            expect(response.status).equals(204)
+    })
+    });
+
+    it('returns a JSON data', () => {
+        cy.request('//api/users')
+          .its('headers')
+          .its('content-type')
+          .should('include', 'application/json');
+      });
+    
+    it('loads all the Users', () => {
+        cy.api({ url: '/api/users?page=2' }).then((res) => {
+          expect(res.status).to.equal(200);
+        });
+      });
+    
+    
+      it('should add a new User successfully', () => {
+        let userDetails = {
+            "name": "ramya",
+            "job": "sdet"
+        }
+        cy.api({
+          method: 'POST',
+          url: '/api/users',
+          body: userDetails
+        }).then((response) => {
+          expect(response.status).to.equal(201);
+          expect(response.body.name).to.equal('ramya');
+          expect(response.body.job).to.equal('sdet');
+        });
+      });
+});
+
